ededfa1acc0e1b0638cccfc19f7b73f79893991e
Eric Morphis <emorphis@stripe.com>
2020-04-07 13:09:51 -0700

    web/python implementation of payment sample

---
+++ server/python/server.py
@@ -0,0 +1,77 @@
+#! /usr/bin/env python3.6
+
+"""
+server.py
+Stripe Sample.
+Python 3.6 or newer required.
+"""
+
+import json
+import os
+import random
+import string
+
+import stripe
+from dotenv import load_dotenv, find_dotenv
+from flask import Flask, jsonify, render_template, redirect, request, session, send_from_directory
+import urllib
+
+# Setup Stripe python client library
+load_dotenv(find_dotenv())
+stripe.api_key = os.getenv('STRIPE_SECRET_KEY')
+stripe.api_version = os.getenv('STRIPE_API_VERSION', '2019-12-03')
+
+static_dir = str(os.path.abspath(os.path.join(__file__ , "..", os.getenv("STATIC_DIR"))))
+app = Flask(__name__, static_folder=static_dir,
+            static_url_path="", template_folder=static_dir)
+
+# Set the secret key to some random bytes. Keep this really secret!
+# This enables Flask sessions.
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+
+@app.route('/', methods=['GET'])
+def get_example():
+    return render_template('index.html')
+
+
+def calculate_order_amount(items):
+    # Replace this constant with a calculation of the order's amount
+    # Calculate the order total on the server to prevent
+    # people from directly manipulating the amount on the client
+    return 1400
+
+
+@app.route('/create-payment-intent', methods=['POST'])
+def create_payment():
+    data = json.loads(request.data)
+    print(data)
+    # Create a PaymentIntent with the order amount and currency
+    intent = stripe.PaymentIntent.create(
+        amount=calculate_order_amount(data['items']),
+        currency=data['currency'],
+        transfer_data={'destination': data['account']}
+    )
+
+    try:
+        # Send publishable key and PaymentIntent details to client
+        return jsonify({'publishableKey': os.getenv('STRIPE_PUBLISHABLE_KEY'), 'clientSecret': intent.client_secret})
+    except Exception as e:
+        return jsonify(error=str(e)), 403
+
+
+@app.route("/recent-accounts", methods=["GET"])
+def get_accounts():
+    accounts = stripe.Account.list(limit=10)
+    return jsonify({'accounts': accounts})
+
+
+@app.route("/express-dashboard-link", methods=["GET"])
+def get_express_dashboard_link():
+    account_id = request.args.get('account_id')
+    link = stripe.Account.create_login_link(account_id, redirect_url=(request.url_root))
+    print(request.url_root)
+    return jsonify({'url': link.url})
+
+
+if __name__== '__main__':
+    app.run(port=4242)
